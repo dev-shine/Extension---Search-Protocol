0312e555aa5450530b2aa3caa1dc9e97cd2371cf
robbieve <robbieeve751994@gmail.com>
2018-04-23 13:57:52 +0800

    footer

--- src/popup/app.js
+++ src/popup/app.js
@@ -2,22 +2,30 @@ import React, { Component } from 'react'
 import { HashHistory as Router, Route, Link, Switch, Redirect } from 'react-router-dom'
 
 import Header from './components/header'
+import Footer from './components/footer'
 import routes from './routes'
 import 'antd/dist/antd.css'
 import './app.scss'
 
 class App extends Component {
+  renderContent () {
+    return (
+      <section className="content">
+        <Switch>
+          {routes.map((route) => (
+            <Route {...route} key={route.path} />
+          ))}
+        </Switch>
+      </section>
+    )
+  }
+
   render () {
     return (
       <div className="app">
         <Header />
-        <section className="content">
-          <Switch>
-            {routes.map((route) => (
-              <Route {...route} key={route.path} />
-            ))}
-          </Switch>
-        </section>
+        {this.renderContent()}
+        <Footer />
       </div>
     );
   }
