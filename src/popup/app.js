1d38644e3cdaf649c398c3c53f8586c334cf260f
robbieve <robbieeve751994@gmail.com>
2018-04-23 13:50:12 +0800

    header

--- src/popup/app.js
+++ src/popup/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { HashHistory as Router, Route, Link, Switch, Redirect } from 'react-router-dom'
 
+import Header from './components/header'
 import routes from './routes'
 import 'antd/dist/antd.css'
 import './app.scss'
@@ -9,6 +10,7 @@ class App extends Component {
   render () {
     return (
       <div className="app">
+        <Header />
         <section className="content">
           <Switch>
             {routes.map((route) => (
