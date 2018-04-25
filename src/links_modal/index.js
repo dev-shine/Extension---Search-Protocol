5cf0412e653f48a26373a6d7456edf8ec9b414c8
kdepp <kdepp.cd@gmail.com>
2018-04-25 21:03:16 +0800

    basic links modal

---
+++ src/links_modal/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+
+const rootEl = document.getElementById('root');
+const render = () => ReactDOM.render(<App />, rootEl)
+
+render()
