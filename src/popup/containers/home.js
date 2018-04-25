f3c50acae4e5922a3f9bcf357a567cc29be55b06
robbieve <robbieeve751994@gmail.com>
2018-04-25 10:17:51 +0800

    UI for create link page

--- src/popup/containers/home.js
+++ src/popup/containers/home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { bindActionCreators }  from 'redux'
-import { Alert, Button } from 'antd'
+import { Alert, Button, Select, Form, Input } from 'antd'
 
 import './home.scss'
 import * as actions from '../actions'
@@ -10,6 +10,11 @@ import { compose } from '../../common/utils'
 import UserInfo from '../components/user_info'
 import ImageForm from '../components/image_form'
 import API from '../../common/api/popup_api'
+
+const relationships = [
+  'Supports', 'Refutes', 'Models', 'Aggregates',
+  'is Example of', 'is Metaphor for', 'is Instance of', 'is Member of'
+]
 
 class Home extends React.Component {
   componentDidMount () {
@@ -72,7 +77,7 @@ class Home extends React.Component {
 
   renderAnnotatedOne () {
     return (
-      <div className="annotate-1">
+      <div className="annotate-1 with-annotation">
         <div className="two-annotation">
           <div className="annotate-item">
             <ImageForm
@@ -112,12 +117,136 @@ class Home extends React.Component {
     )
   }
 
+  renderAnnotatedTwo () {
+    return (
+      <div className="annotate-2 with-annotation">
+        <div className="two-annotation">
+          <div className="annotate-item">
+            <ImageForm
+              image="http://h.hiphotos.baidu.com/image/h%3D300/sign=d9d2e0ddb5014a909e3e40bd99763971/21a4462309f790525fe7185100f3d7ca7acbd5e1.jpg"
+            />
+          </div>
+          <div className="annotate-item">
+            <ImageForm
+              image="http://h.hiphotos.baidu.com/image/h%3D300/sign=d9d2e0ddb5014a909e3e40bd99763971/21a4462309f790525fe7185100f3d7ca7acbd5e1.jpg"
+            />
+          </div>
+        </div>
+        <div className="actions">
+          <Button
+            type="primary"
+            size="large"
+            className="create-link-button"
+            onClick={() => {
+              console.log('todo: create link')
+            }}
+          >
+            Build Link
+          </Button>
+          <Button
+            type="danger"
+            size="large"
+            className="cancel-button"
+            onClick={() => {
+              console.log('todo: cancel')
+            }}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
+  renderCreateLink () {
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <div className="to-create-link">
+        <h2>Create Link</h2>
+        <Form onSubmit={this.handleSubmit} className="create-link-form">
+          <Form.Item label="How are these links related?">
+            <div className="relationship-row">
+              <div className="image-box">
+                <img src="http://h.hiphotos.baidu.com/image/h%3D300/sign=d9d2e0ddb5014a909e3e40bd99763971/21a4462309f790525fe7185100f3d7ca7acbd5e1.jpg" />
+              </div>
+
+              <div>
+                {getFieldDecorator('relationship', {
+                  rules: [
+                    { required: true, message: 'Please select relation' }
+                  ]
+                })(
+                  <Select placeholder="Choose a relationship">
+                    {relationships.map(r => (
+                      <Select.Option key={r} value={r}>{r}</Select.Option>
+                    ))}
+                  </Select>
+                )}
+              </div>
+
+              <div className="image-box">
+                <img src="http://h.hiphotos.baidu.com/image/h%3D300/sign=d9d2e0ddb5014a909e3e40bd99763971/21a4462309f790525fe7185100f3d7ca7acbd5e1.jpg" />
+              </div>
+            </div>
+          </Form.Item>
+
+          <Form.Item label="What do you want to say about this link?">
+            {getFieldDecorator('desc', {
+              validateTrigger: ['onBlur'],
+              rules: [
+                { required: true, message: 'Please input description' }
+              ]
+            })(
+              <Input.TextArea placeholder="Enter Description For This Link" />
+            )}
+          </Form.Item>
+          <Form.Item label="Tags">
+            {getFieldDecorator('tags', {
+              validateTrigger: ['onBlur'],
+              rules: [
+                { required: true, message: 'Please input tags' }
+              ]
+            })(
+              <Input placeholder="Supporting information, opposing information, data, another perspective, etc." />
+            )}
+          </Form.Item>
+        </Form>
+
+        <div className="actions">
+          <Button
+            type="primary"
+            size="large"
+            className="post-button"
+            onClick={() => {
+              console.log('todo: post')
+            }}
+          >
+            POST IT!
+          </Button>
+          <Button
+            type="danger"
+            size="large"
+            className="cancel-button"
+            onClick={() => {
+              console.log('todo: cancel')
+            }}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
   renderContent () {
     const { userInfo } = this.props
 
     if (!userInfo)  return null
 
-    return this.renderAnnotatedOne()
+    return this.renderCreateLink()
+
+    return this.renderAnnotatedTwo()
 
     if (userInfo.user_activate === '0') {
       return this.renderUserNotActivated()
@@ -143,5 +272,6 @@ export default compose(
     state => ({ userInfo: state.userInfo }),
     dispatch => bindActionCreators({...actions}, dispatch)
   ),
-  withRouter
+  withRouter,
+  Form.create()
 )(Home)
