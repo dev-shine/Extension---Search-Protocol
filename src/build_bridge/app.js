b2b179f29445a207391f091ba98e14359869948e
kdepp <kdepp.cd@gmail.com>
2018-04-28 22:21:11 +0800

    build bridge page

--- src/build_bridge/app.js
+++ src/build_bridge/app.js
@@ -3,43 +3,57 @@ import { Modal, Select, Form, Input, Button } from 'antd'
 import { notifyError, notifySuccess } from '../components/notification'
 import { ipcForIframe } from '../common/ipc/cs_postmessage'
 import API from '../common/api/cs_iframe_api'
+import { compose, setIn, updateIn } from '../../common/utils'
+import CreateLinkComp from '../components/create_link'
 import './app.scss'
 
 const ipc = ipcForIframe()
 
 class App extends Component {
   state = {
+    linkPair: null
   }
 
   onClose = () => {
     ipc.ask('CLOSE')
   }
 
+  onUpdateField = (val, field) => {
+    this.setState(
+      setIn(['linkPair', 'data', field], val, this.state)
+    )
+  }
+
+  onClickSubmit = (data) => {
+    API.postLinks(data)
+    .then(() => {
+      notifySuccess('Successfully posted')
+      setTimeout(() => this.onClickCancel(), 1500)
+    })
+    .catch(e => {
+      notifyError(e.message)
+    })
+  }
+
+  onClickCancel = () => {
+    API.setLinkPair({ links: [], desc: null, tags: null })
+  }
+
   componentDidMount () {
     API.getLinkPairStatus()
-    .then(({ status, data }) => {
-      this.setState({
-        links:  data.links || [],
-        desc:   data.desc || '',
-        tags:   data.tags || '',
-        relationship: data.relationship || ''
-      })
+    .then(linkPair => {
+      this.setState({ linkPair })
     })
-    // ipc.ask('INIT')
-    // .then(pairs => {
-    //   console.log('init got pairs', pairs)
-
-    //   this.setState({
-    //     pairs,
-    //     pid: pairs.length > 0 ? pairs[0].id : null
-    //   })
-    // })
   }
 
   render () {
-    const pair = this.state.pairs.find(p => p.id === this.state.pid) || {}
-
     return (
+      <CreateLinkComp
+        linkPair={this.state.linkPair}
+        onUpdateField={this.onUpdateField}
+        onSubmit={this.onClickSubmit}
+        onCancel={this.onClickCancel}
+      />
     )
   }
 }
