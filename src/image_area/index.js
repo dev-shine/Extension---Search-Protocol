83d1433b1878850c1de1b87c793bf64d60ac4a36
KDepp <kdepp.cd@gmail.com>
2018-05-03 21:37:20 +0800

    pass wheel event from iframe to parent

--- src/image_area/index.js
+++ src/image_area/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
+import ipc from '../common/ipc/ipc_dynamic'
+import { passWheelEvent } from '../common/ipc/cs_postmessage'
+
+if (window.top !== window) {
+  passWheelEvent(ipc)
+}
 
 const rootEl = document.getElementById('root');
 const render = () => ReactDOM.render(<App />, rootEl)
