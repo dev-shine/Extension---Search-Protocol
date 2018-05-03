5cb83a648728b5532fc0980af7187a919e22f451
KDepp <kdepp.cd@gmail.com>
2018-05-03 17:40:56 +0800

    background: init user settings if it's empty

--- src/extension/scripts/bg.js
+++ src/extension/scripts/bg.js
@@ -1,11 +1,26 @@
 import Ext from '../../common/web_extension'
 import { delay } from '../../common/utils'
 import API from '../../common/api/bg_api'
+import log from '../../common/log'
 import { getTabIpcstore } from '../../common/tab_ipc_store'
 import { bgInit as popupBgInit } from '../../common/ipc/ipc_bg_popup'
 import { bgInit as csBgInit } from '../../common/ipc/ipc_bg_cs'
 
 const tabIpcStore = getTabIpcstore()
+
+const init = () => {
+  initUserSettings()
+  bindEvents()
+}
+
+const initUserSettings = () => {
+  API.getUserSettings()
+  .then(data => {
+    if (data) return
+    return API.resetUserSettings()
+  })
+  .catch(e => log.error(e.stack))
+}
 
 const bindEvents = () => {
   popupBgInit(ipc => ipc.onAsk(onPopupRequest))
@@ -23,4 +38,4 @@ const onCsRequest = (cmd, args) => {
 
 }
 
-bindEvents()
+init()
