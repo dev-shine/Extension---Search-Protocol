f8c61f58694e9efee2e298e196753c606beb6c1d
robbieve <robbieeve751994@gmail.com>
2018-05-05 22:11:36 +0800

    data shape check for 3 models

--- src/common/models/annotation_model.js
+++ src/common/models/annotation_model.js
@@ -1,8 +1,21 @@
 import { BaseModel, createLocalBackend } from './base_model'
+import { ObjectWith } from '../type_check'
 
 const backend = createLocalBackend('annotations')
 
+const annotationShape = new ObjectWith({
+  // target is id of element
+  target: String,
+  title:  String,
+  desc:   String,
+  tags:   String
+})
+
 export class AnnotationModel extends BaseModel {
+  shapeOfData (data) {
+    return annotationShape
+  }
+
   commit (data) {
     return backend.commit(data)
   }
--- src/common/models/element_model.js
+++ src/common/models/element_model.js
@@ -1,8 +1,44 @@
 import { BaseModel, createLocalBackend } from './base_model'
+import { TARGET_TYPE } from './link_pair_model'
+import { ObjectWith } from '../type_check'
 
 const backend = createLocalBackend('elements')
 
+const rectShape  = new ObjectWith({
+  x:        Number,
+  y:        Number,
+  width:    Number,
+  height:   Number
+})
+const selPosShape = new ObjectWith({
+  locator:  String,
+  offset:   Number
+})
+const imageShape = new ObjectWith({
+  type:     String,
+  url:      String,
+  locator:  String,
+  image:    String,
+  rect:     rectShape
+})
+const selectionShape = new ObjectWith({
+  type:     String,
+  url:      String,
+  start:    selPosShape,
+  end:      selPosShape,
+  text:     String
+})
+
 export class ElementModel extends BaseModel {
+  shapeOfData (data) {
+    switch (data.type) {
+      case TARGET_TYPE.IMAGE:       return imageShape
+      case TARGET_TYPE.SELECTION:   return selectionShape
+      default:
+        throw new Error(`Element shapeOfData: unknow type - '${data.type}'`)
+    }
+  }
+
   commit (data) {
     return backend.commit(data)
   }
