4bfa33d471004a7806cf158e7dba3c92c93c9fbe
robbieve <robbieeve751994@gmail.com>
2018-05-11 22:34:44 +0800

    broadcast settings change

--- src/extension/scripts/bg.js
+++ src/extension/scripts/bg.js
@@ -1,4 +1,5 @@
 import Ext from '../../common/web_extension'
+import storage from '../../common/storage'
 import { delay } from '../../common/utils'
 import API from '../../common/api/bg_api'
 import log from '../../common/log'
@@ -27,6 +28,13 @@ const bindEvents = () => {
   csBgInit((tabId, ipc) => {
     tabIpcStore.set(tabId, ipc)
     ipc.onAsk(onCsRequest)
+  })
+  storage.addListener(changes => {
+    const settingsChange = changes.find(c => c.key === 'user_settings')
+    if (!settingsChange)  return
+
+    const settings = settingsChange.newValue
+    tabIpcStore.forEach(ipc => ipc.ask('UPDATE_SETTINGS', { settings }))
   })
 }
 
