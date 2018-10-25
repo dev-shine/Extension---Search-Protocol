b381a46a00ede50f175cb3c98d6146ce608931bc
robbieve <robbieeve751994@gmail.com>
2018-10-25 18:54:40 +0530

    baseUrl set to live

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,8 @@ export default {
   localBackend: false,
   api: {
     // base: __DEVELOPMENT__ ? 'http://bridgit.test/api' : 'https://bridgit.io/app/api',
-    // base: 'https://bridgit.io/app/api'
-    base: 'http://172.16.1.48:8000/api'
+    base: 'https://bridgit.io/app/api'
+    // base: 'http://172.16.1.48:8000/api'
   },
   settings: {
     minImageAreaRatio: 0.05,
